e4315510df435301409e3c38c7e66de458c3d48a
kali20177 <39610949+kali20177@users.noreply.github.com>
2021-03-07 20:53:40 +0800

    Create stack-array.cpp

---
+++ stack-array.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX 1000
+
+class Stack
+{
+	int top;
+public:
+	int a[MAX];
+
+	Stack() { top = -1; }
+	bool push(int x);
+	int pop();
+	int peek();
+	bool isempty();
+};
+bool Stack::push(int x)
+{
+	if ( top >= (MAX - 1) )
+	{
+		cout << "Stack Overflow" << endl;
+		return false;
+	}
+	else
+	{
+		a[++ top] = x;
+		cout << x << " pushed into stack" << endl;
+		return true;
+	}
+}
+
+int Stack::pop()
+{
+	if (top < 0){
+		cout << "Stack Underflow" << endl;
+		return 0;
+	}
+	else{
+		int x = a[top --];
+		return x;
+	}
+}
+bool Stack::isempty() 
+{
+	return (top < 0);
+}
+int main()
+{
+	class Stack s;
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	cout << s.pop() << " Popped from stack" << endl;
+	return 0;
+}
